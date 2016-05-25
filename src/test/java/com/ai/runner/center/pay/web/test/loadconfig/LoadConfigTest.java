3f7e175eed9ff0eb8b248c5d822d076d0375396a
LiangMeng <liangmeng@asiainfo.com>
2016-05-25 11:03:51 +0800

    初始化slp-pay-web

---
+++ src/test/java/com/ai/runner/center/pay/web/test/loadconfig/LoadConfigTest.java
@@ -0,0 +1,22 @@
+package com.ai.runner.center.pay.web.test.loadconfig;
+
+import java.util.ResourceBundle;
+
+import org.junit.Test;
+
+import com.ai.runner.sdk.loader.Load2ConfigCenter;
+
+public class LoadConfigTest {
+
+    public static void main(String[] args) {
+        Load2ConfigCenter.main(new String[]{"F:\\asiainfo\\myJob\\test"});
+    }
+    
+    @Test
+    public void testResouceBundle() {
+        ResourceBundle rb = ResourceBundle.getBundle("appsvr_conf");
+        String value = rb.getString("appId");
+        System.out.println(value);
+    }
+        
+}
